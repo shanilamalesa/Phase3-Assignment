7293c2504f4b0e539665ae6501ccaa3914cdf817
shanila malesa <shanilamalesa01@icloud.com>
2026-08-16 17:05:34 +0100

    fix: changing the shop name to MCTABA SHOP

--- shop-next/app/layout.js
+++ shop-next/app/layout.js
@@ -4,7 +4,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Mctaba Shop",
+  title: "MCTABA SHOP",
   description: "Your local online store for quality skincare",
 };
 
